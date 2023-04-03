246a349aa89db9c65d33ea0e959c8453b0cab45d
ofeefo <felipe.mtt95@gmail.com>
2023-04-03 19:57:07 -0300

    refactor(plugin/run): Implement Run

--- plugin/run.go
+++ plugin/run.go
@@ -3,11 +3,32 @@ package plugin
 import "github.com/cocov-ci/go-plugin-kit/cocov"
 
 func Run(ctx cocov.Context) error {
-	res, err := run(ctx)
+	out, err := run(ctx)
 	if err != nil {
 		return err
 	}
-	_ = res
+
+	sha := ctx.CommitSHA()
+	for _, res := range out.Results {
+		for _, m := range res.Messages {
+
+			kind, ok := rules[m.RuleID]
+			if !ok {
+				continue
+			}
+
+			input := fmt.Sprintf(
+				"%s-%d-%s-%s",
+				kind.String(), m.Line, res.FilePath, sha,
+			)
+
+			id := cocov.SHA1([]byte(input))
+			if err = ctx.EmitIssue(kind, res.FilePath, m.Line, m.EndLine, m.Message, id); err != nil {
+				ctx.L().Error("Error emitting issue", zap.Error(err))
+				return err
+			}
+		}
+	}
 
 	return nil
 }
@@ -24,7 +45,7 @@ func run(ctx cocov.Context) ([]result, error) {
 		return nil, err
 	}
 
-	if err = restoreNodeModules(ctx, exec, mgr, ""); err != nil {
+	if err = restoreNodeModules(ctx, exec, mgr, np); err != nil {
 		return nil, err
 	}
 
