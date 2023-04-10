b74479415b9ca2e18e4ec08b2859e0b1538d7b1f
ofeefo <felipe.mtt95@gmail.com>
2023-04-10 20:08:23 -0300

    refactor(plugin): Move runEslint to a separate file

---
+++ plugin/eslint.go
@@ -0,0 +1,55 @@
+package plugin
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cocov-ci/go-plugin-kit/cocov"
+	"go.uber.org/zap"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+func runEslint(ctx cocov.Context, e Exec, nodePath string) (*cliOutput, error) {
+	wd := ctx.Workdir()
+	eslintPath := filepath.Join(wd, "node_modules", ".bin", "eslint")
+	args := []string{"-f", "json-with-metadata", "."}
+
+	ctx.L().Info("Running eslint")
+	start := time.Now()
+
+	envs := map[string]string{"PATH": nodePath}
+	opts := &cocov.ExecOpts{Workdir: wd, Env: envs}
+	stdOut, stdErr, err := e.Exec2(eslintPath, args, opts)
+	if err != nil {
+		if execErr, ok := err.(*exec.ExitError); ok {
+			if execErr.ExitCode() != 1 {
+				ctx.L().Error("eslint exited with unexpected status",
+					zap.Int("status", execErr.ExitCode()),
+					zap.String("std err", string(stdErr)),
+					zap.Error(err),
+				)
+				return nil, err
+			}
+
+		} else if !ok {
+			ctx.L().Error("error running eslint",
+				zap.String("std err", string(stdErr)),
+				zap.Error(err),
+			)
+			return nil, err
+		}
+	}
+
+	msg := fmt.Sprintf("Running eslint took %s seconds", time.Since(start))
+	ctx.L().Info(msg)
+
+	out := &cliOutput{}
+	if err = json.Unmarshal(stdOut, out); err != nil {
+		ctx.L().Error("failed to unmarshall output",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	return out, nil
+}
--- plugin/node-modules.go
+++ plugin/node-modules.go
@@ -1,12 +1,6 @@
 package plugin
 
 import (
-	"encoding/json"
-	"fmt"
-	"os/exec"
-	"path/filepath"
-	"time"
-
 	"github.com/cocov-ci/go-plugin-kit/cocov"
 	"go.uber.org/zap"
 )
@@ -40,48 +34,4 @@ func restoreNodeModules(ctx cocov.Context, e Exec, manager, file, nodePath strin
 	}
 
 	return nil
-}
-
-func runEslint(ctx cocov.Context, e Exec, nodePath string) (*cliOutput, error) {
-	wd := ctx.Workdir()
-	eslintPath := filepath.Join(wd, "node_modules", ".bin", "eslint")
-	args := []string{"-f", "json-with-metadata", "."}
-
-	ctx.L().Info("Running eslint")
-	start := time.Now()
-
-	envs := map[string]string{"PATH": nodePath}
-	opts := &cocov.ExecOpts{Workdir: wd, Env: envs}
-	stdOut, stdErr, err := e.Exec2(eslintPath, args, opts)
-	if err != nil {
-		if execErr, ok := err.(*exec.ExitError); ok {
-			if execErr.ExitCode() != 1 {
-				ctx.L().Error("eslint exited with unexpected status",
-					zap.Int("status", execErr.ExitCode()),
-					zap.String("std err", string(stdErr)),
-					zap.Error(err),
-				)
-				return nil, err
-			}
-
-		} else if !ok {
-			ctx.L().Error("error running eslint",
-				zap.String("std err", string(stdErr)),
-				zap.Error(err),
-			)
-			return nil, err
-		}
-	}
-
-	msg := fmt.Sprintf("Running eslint took %s seconds", time.Since(start))
-	ctx.L().Info(msg)
-
-	out := &cliOutput{}
-	if err = json.Unmarshal(stdOut, out); err != nil {
-		ctx.L().Error("failed to unmarshall output",
-			zap.Error(err),
-		)
-		return nil, err
-	}
-	return out, nil
 }
